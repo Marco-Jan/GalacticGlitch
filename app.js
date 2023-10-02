3af1b3dc97bd10646235ea0be3e9e0fe965aa15a
Iron-Baloou <marco.jan@gmx.de>
2023-10-03 00:03:44 +0200

    init commit

--- app.js
+++ app.js
@@ -1,32 +1,62 @@
 class Game {
-    constructor({color}){
+    constructor({ color }) {
+        this.background = color;
+        this.character = new Character();
+        this.obstacles = new Obstacles();
+    }
 
-        this.height = 768,
-        this.width = 1024,
-        this.background = color,
-        this.charakter = new Charakter,
-        this.obstacles = new Obstacles
-        
+    draw() {
+        const container = document.querySelector('#container');
+        container.style.backgroundColor = this.background;
+
+        const characterElement = document.querySelector('#character');
+        characterElement.style.left = `${this.character.x}px`;
+        characterElement.style.top = `${this.character.y}px`;
     }
 }
 
+class Character {
+    constructor() {
+        this.x = 50;  // starting x-coordinate
+        this.y = 50;  // starting y-coordinate
+        this.velocity = 0;  // initial velocity
+    }
 
-class Charakter {
-    constructor({}){
-
+    move() {
+        this.velocity += 1;  // gravity
+        this.y += this.velocity;  // update y-coordinate based on velocity
 
     }
 }
-
 
 class Obstacles {
-    constructor({}){
+    constructor() {
+        this.pipes = [];  // array to hold pipe objects
+    }
 
+    generate() {
+        // Code to generate new pipes
+    }
 
+    move() {
+        // Code to move the pipes
     }
 }
 
-const newGame = new Game ({
+let newGame;
 
-    color: 'red'
-})
+function initialGame() {
+    newGame = new Game({
+        color: 'red'
+
+    });
+
+    newGame.draw()
+}
+
+initialGame();
+console.log(newGame.background);
+
+// Example: Updating character's position
+newGame.character.move();
+console.log(newGame.character.y);  // Should log 51, since the initial y-coordinate was 50
